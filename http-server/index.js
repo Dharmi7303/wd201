aea55437c83384282d8ecdae71bc8f2994139c21
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2023-03-01 22:58:33 +0530

    Update index.js

--- http-server/index.js
+++ http-server/index.js
@@ -1,48 +1,61 @@
-const form_validate=()=>{
-    const name=document.getElementById("name").value;
-    const email=document.getElementById("email").value;
-    const password=document.getElementById("password").value;
-    const dob=document.getElementById("dob").value;
-    const acceptterms=document.getElementById("acceptTerms").checked;
-    var Entries;
-    if(localStorage.getItem('user-entries')===null)
-    Entries=[];
-    else
-    Entries=JSON.parse(localStorage.getItem('user-entries'))
-    Entries.unshift({
-    name:name,
-    email:email,
-    password:password,
-    dob:dob,
-    acceptterms:acceptterms
-    })
-    localStorage.setItem('user-entries',JSON.stringify(Entries));
-    showdata()
-    document.getElementById('name').value="";
-    document.getElementById('email').value="";
-    document.getElementById('password').value="";
-    document.getElementById('dob').value="";
-    document.getElementById('acceptTerms').value="true";
+const http = require("http");
+const fs = require("fs");
+const minimist = require("minimist");
+
+const args = minimist(process.argv.slice(2), {
+    alias: {
+        port: "p",
     }
-    const showdata=()=>{
-    var Entries;
-    if(localStorage.getItem('user-entries')===null)
-    Entries=[];
-    else
-    Entries=JSON.parse(localStorage.getItem('user-entries'))
-    
-    var html="";
-    
-    Entries.forEach((element,intex)=>{
-    html += "<tr>";
-    html += '<td>' + element.name+"</td>";
-    html += '<td>' + element.email+"</td>";
-    html += '<td>' + element.password+"</td>";
-    html += '<td>' + element.dob+"</td>";
-    html += '<td>' + element.acceptterms+"</td>";
-    html += "</tr>";
-    });
-    document.querySelector("#regTable tbody").innerHTML=html;
-    
+});
+
+const port = args.port || 3000;
+
+
+let homeContent = "";
+let projectContent = "";
+let registrationContent = "";
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html", (err, project) => {
+    if (err) {
+      throw err;
     }
-    document.onload=showdata();
+    registrationContent = project;
+  });
+
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationContent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(port, () => {
+    console.log(`Server running on port ${port}`);
+  });
+ 
