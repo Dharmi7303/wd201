0b8db1196672b0ccc74a4aaa432ee255de39e3f7
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2023-04-05 21:15:15 +0530

    Update todo.js

--- todo-app/models/todo.js
+++ todo-app/models/todo.js
@@ -1,5 +1,5 @@
 "use strict";
-const { Model } = require("sequelize");
+const { Model, Op } = require("sequelize");
 module.exports = (sequelize, DataTypes) => {
   class Todo extends Model {
     /**
@@ -7,32 +7,32 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-
-    // static associate(models) {
-    //   // define association here
-
-    // }
+    static associate(models) {
+      // define association here
+    }
 
     static addTodo({ title, dueDate }) {
       return this.create({ title: title, dueDate: dueDate, completed: false });
-    }
-
-    static getAllTodos() {
-      return this.findAll();
     }
 
     markAsCompleted() {
       return this.update({ completed: true });
     }
 
-    static async deleteTodo(id) {
-      return (await this.destroy({ where: { id } })) == 1 ? true : false;
+    static async odue(){
+      return this.findAll({where:{dueDate:{[Op.lt] : new Date()},},});
+    }
+    static async tdue(){
+      return this.findAll({where:{dueDate:{[Op.eq] : new Date()},},});
+    }
+    static async ldue(){
+      return this.findAll({where:{dueDate:{[Op.gt] : new Date()},},});
     }
   }
   Todo.init(
     {
       title: DataTypes.STRING,
-      dueDate: DataTypes.STRING,
+      dueDate: DataTypes.DATEONLY,
       completed: DataTypes.BOOLEAN,
     },
     {
