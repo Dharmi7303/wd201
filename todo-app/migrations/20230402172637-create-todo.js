977c26489521d54fee1d03fe7e305a9a08051ec8
Dharmi Javiya <92503014+Dharmi7303@users.noreply.github.com>
2023-04-25 14:29:18 +0530

    Add files via upload

---
+++ todo-app/migrations/20230402172637-create-todo.js
@@ -0,0 +1,41 @@
+"use strict";
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable("Todos", {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+          notNull: true,
+          len: 5,
+        },
+      },
+      dueDate: {
+        type: Sequelize.DATEONLY,
+        allowNull: false,
+      },
+      completed: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: false,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable("Todos");
+  },
+};
